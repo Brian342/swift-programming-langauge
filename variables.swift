ed69441ffcb4a8fe4c7e364e059ca17fc81645df
Brian kimanzi <migelbrian3@gmail.com>
2025-10-14 04:27:27 +0300

    new version

--- variables.swift
+++ variables.swift
@@ -1,4 +1,9 @@
 import Foundation
+// let creates immutability i.e an not be assigned again
+let myName = "James" 
 
-let // creates immutability i.e an not be assigned again
-var // creates mutability i.e can be assigned again
+// creates mutability i.e can be assigned again
+var yourName = "Paul"
+
+myName = yourName
+print(myName)
