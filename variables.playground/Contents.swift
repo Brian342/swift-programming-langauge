4c50b0707bff6afe64dfc57b41b68c3933c3c5ba
Brian kimanzi <migelbrian3@gmail.com>
2025-10-15 00:44:29 +0300

    new code update

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+let myName = "James"
+var yourName = "Foo"
+
+
+var names = [
+    myName,
+    yourName
+]
+names.append("Bar")
+names.append("Baz")
+
+let foo = "Foo"
+var foo2 = foo
+
+foo2 = "Foo 2"
+foo
+foo2
+
+let moreNames = [
+    "Foo",
+    "Bar"
+]
+var copy = moreNames
+copy.append("Bar")
+moreNames
+copy
+
+let oldArray = NSMutableArray(
+    array: [
+    "Foo",
+    "Bar"
+    ]
+)
+oldArray.add("Baz")
+var newArray = oldArray
+newArray.add("Qux")
+oldArray
+newArray
