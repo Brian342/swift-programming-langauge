eca5d7e2adde40a67d6b6b8c26a07a1cd9e157f9
Brian kimanzi <migelbrian3@gmail.com>
2025-10-15 01:29:49 +0300

    new code update

--- if else.playground/Contents.swift
+++ if else.playground/Contents.swift
@@ -1,3 +1,20 @@
 import Foundation
 
+let myName = "Vandad"
+let myAge = 20
+let yourName = "Foo"
+let yourAge = 19
 
+if myName == "vandad"{
+    "your name is \(myName)"
+}else {
+    "oops, i guessed it wrong"
+}
+
+if myName == "Vandad"{
+    "Now i guessed it correctly"
+}else if myName == "Foo"{
+    "Are you Foo"
+}else{
+    "Okay! i give up"
+}
