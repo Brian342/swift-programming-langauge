d49d0e3314d87bec2b30aeb500150957bb7b2fec
Brian kimanzi <migelbrian3@gmail.com>
2025-10-15 01:42:41 +0300

    new code update

--- if else.playground/Contents.swift
+++ if else.playground/Contents.swift
@@ -18,3 +18,24 @@ if myName == "Vandad"{
 }else{
     "Okay! i give up"
 }
+
+if myName == "Vandad" && myAge  == 30 {
+    "Name is Vandad and age is 30"
+}else if myAge == 20 {
+    "I only guessed the age right"
+}else{
+    "I dont know what i'm doing"
+}
+
+if myAge == 20 || myName == "Foo"{
+    "Either age is 20, name is Foo or Both"
+}else if myName == "Vandad" || myAge == 20{
+    "it's too late to get in this clause"
+}
+
+if myName == "Vandad"
+    && myAge == 22
+    && yourName == "Foo"
+    || yourAge == 19{
+    "my name is vandad and i'm 22 and your name is Foo..or..you are 19"
+}
